639f4e99b16a76f9c9291dcc1d02179edb7bc923
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-05-18 15:18:25 +0800

    feat(permissions): implement role-based access control
    This commit implements role-based access control (RBAC) using Spatie's Laravel Permission package.
    
    The following changes were made:
    
    - Created a `PermissionSeeder` to seed permissions and roles.
    - Created an administrator role with all permissions.
    - Created a test user and assigned the administrator role.
    - Updated the `DatabaseSeeder` to call the `PermissionSeeder`.
    
    This implementation provides a flexible and granular approach to managing user permissions within the application.

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Closure;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class PermissionSeeder extends Seeder
 {
@@ -42,7 +47,56 @@ class PermissionSeeder extends Seeder
             return ['name' => $permission, 'guard_name' => 'web'];
         });
 
-        Permission::insert($permissions->toArray());
+        /**
+         * Create Permissions
+         */
+        $this->command->warn(PHP_EOL.'Creating permissions...');
+        $this->withProgressBar(1, fn () => Permission::insert($permissions->toArray()));
+        $this->command->info('Permissions has been created.');
 
+        /**
+         * Create Roles
+         */
+        $this->command->warn(PHP_EOL.'Creating roles...');
+        $this->withProgressBar(1, function () use ($allPermissionNames) {
+            $role = Role::create(['name' => 'administrator']);
+            $role->givePermissionTo($allPermissionNames->flatten()->toArray());
+        });
+        $this->command->info('Roles has been created.');
+
+        /**
+         *  Create User
+         */
+        $this->command->warn(PHP_EOL.'Creating user and assigning roles...');
+        $this->withProgressBar(1, function () {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ])->assignRole('administrator');
+        });
+        $this->command->info('Administrator user created.');
+
+    }
+
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection();
+
+        foreach (range(1, $amount) as $i) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
