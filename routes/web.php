c540b4e01465ab23aa8756c9621fb59f73a5ac9d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-05-19 17:41:41 +0800

    feat(api): Improve API response format
    Improve the format of the API response by:
    
    - Renaming `laravel_version` to `laravel` for consistency.
    - Renaming `document_version` to `documentation_version` for clarity.

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'laravel_version' => app()->version(),
+        'laravel' => app()->version(),
         'documentation' => '/docs/api',
-        'document_version' => config('scramble.info.version'),
+        'documentation_version' => config('scramble.info.version'),
         'php_version' => PHP_VERSION,
     ]);
 });
