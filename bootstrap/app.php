3bc7a23fda86b48956fe026605ba8889c0ab64c7
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-05-23 21:44:41 +0800

    feat(bootstrap): Handle NotFoundHttpException and HttpException
    Handles `NotFoundHttpException` and `HttpException` to return JSON responses for API requests.
    
    This change introduces custom exception rendering for `NotFoundHttpException` and `HttpException` to provide consistent error responses in JSON format when the API is accessed.
    
    The following improvements are included:
    
    - When a `NotFoundHttpException` is thrown, a JSON response with a "Record Not Found" message and a 404 status code is returned for API requests.
    - Similarly, for other `HttpException` instances, a JSON response containing the exception message and the corresponding status code is returned for API requests.
    - For non-API requests, the default exception handling behavior is preserved.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +24,28 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->shouldRenderJsonWhen(fn (Request $request) => $request->expectsJson() || $request->ajax());
+        $exceptions->stopIgnoring(HttpException::class);
+        $exceptions->render(function (NotFoundHttpException $exception, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'error' => [
+                        'message' => 'Record Not Found',
+                        'code' => $exception->getStatusCode(),
+                    ],
+                ], Response::HTTP_NOT_FOUND);
+            }
+            return $exception;
+        });
+        $exceptions->render(function (HttpException $exception, Request $request) {
+            if ($request->expectsJson()) {
+                return response()->json([
+                    'error' => [
+                        'message' => $exception->getMessage(),
+                        'code' => $exception->getStatusCode(),
+                    ],
+                ], $exception->getStatusCode());
+            }
+            return $exception;
+        });
         //
     })->create();
