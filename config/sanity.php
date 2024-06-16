f13aacb4b8ea2af6fcd091e125456b0281bbc967
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-06-16 22:02:55 +0800

    feat(sanity): add sanity config
    Adds configuration for the Sanity client, including:
    
    - Project ID
    - Dataset
    - API version
    - Token
    
    These settings are loaded from environment variables.

---
+++ config/sanity.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'projectId' => env('SANITY_PROJECT_ID'),
+    'dataset' => env('SANITY_DATASET'),
+    'api_version' => env('', '2024-06-13'),
+    'token' => env('SANITY_TOKEN'),
+];
