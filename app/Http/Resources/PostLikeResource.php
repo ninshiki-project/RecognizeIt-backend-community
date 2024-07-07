d6abe2269586284d71c7f36b52a83596febab9fa
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-07-07 22:14:57 +0800

    feat(PostLikeResource): Include user data by default
    This commit modifies the PostLikeResource to include user data by default.
    
    Previously, the user data had to be explicitly loaded using the `whenLoaded` method. This change simplifies the API response by always including the user information associated with a post like.

--- app/Http/Resources/PostLikeResource.php
+++ app/Http/Resources/PostLikeResource.php
@@ -8,11 +8,14 @@ use Illuminate\Http\Resources\Json\JsonResource;
 /** @mixin \App\Models\PostLike */
 class PostLikeResource extends JsonResource
 {
+    public $with = ['user'];
+
     public function toArray(Request $request): array
     {
         return [
-            'posts_id' => $this->posts_id,
-            'user' => new PostLikeUserResource($this->whenLoaded('user')),
+            'posts_id' => $this->post_id,
+            'user' => new PostLikeUserResource($this->user),
         ];
     }
+
 }
