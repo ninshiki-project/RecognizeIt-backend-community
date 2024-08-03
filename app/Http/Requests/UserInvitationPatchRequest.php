e882ac9b9e162242ee86fbbec3be3767e15f0e3d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-03 18:10:01 +0800

    Makes name and email required only when status is accepted.

--- app/Http/Requests/UserInvitationPatchRequest.php
+++ app/Http/Requests/UserInvitationPatchRequest.php
@@ -10,8 +10,8 @@ class UserInvitationPatchRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required',  'email', 'max:254'],
+            'name' => ['required', 'string', 'max:255', 'sometimes', 'required_if:status,accepted'],
+            'email' => ['required',  'email', 'max:254', 'sometimes', 'required_if:status,accepted'],
             'token' => ['required', 'exists:invitations,token'],
             'status' => ['required', Rule::in(['accepted', 'declined'])],
         ];
