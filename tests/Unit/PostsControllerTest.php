37ce19ad9711187459f6fc836d35d20331e6af68
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-03 21:39:38 +0800

    WIP

---
+++ tests/Unit/PostsControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+it('can like a post', function () {
+
+});
+it('destroy', function () {
+});
+it('store', function () {
+});
+it('show', function () {
+});
+it('update', function () {
+});
+it('can get all posts', function () {
+
+});
+it('can get all 5 post', function () {
+
+});
