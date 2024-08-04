3841d3dc15fe197618d0e8d89a21216846546548
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-04 11:31:25 +0800

    ✨ feat(invitations): added relationships to invitation factory

--- database/factories/InvitationFactory.php
+++ database/factories/InvitationFactory.php
@@ -2,7 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Departments;
 use App\Models\Invitation;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
@@ -17,11 +20,19 @@ class InvitationFactory extends Factory
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
             'email' => $this->faker->unique()->safeEmail(),
-            'invited_by' => $this->faker->word(),
+            'invited_by_user' => User::all()->random(1)->value('id'),
             'token' => Str::random(10),
-            'accepted_at' => Carbon::now(),
-            'status' => $this->faker->word(),
-            'declined_at' => Carbon::now(),
+            'department' => Departments::all()->random(1)->value('id'),
+            'role' => Role::all()->random(1)->value('id'),
         ];
+
+    }
+
+    public function accepted(): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'accepted_at' => Carbon::now(),
+            'status' => 'accepted',
+        ]);
     }
 }
