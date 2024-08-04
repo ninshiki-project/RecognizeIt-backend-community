b24dd197fd50a8689f11e20abc12d93a47b5b02d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-04 10:29:50 +0800

    ♻️ **refactor(tests): Removed unused `createApplication` method**

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,17 +3,11 @@
 namespace Tests;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabaseState;
 use Laravel\Sanctum\Sanctum;
 
 trait CreatesApplication
 {
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
 
     protected function setUp(): void
     {
