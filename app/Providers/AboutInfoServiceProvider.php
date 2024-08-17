9c424439f447e299e17da531b7c873209d137d72
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-17 16:38:06 +0800

    ✨ feat(app): Add Ninshiki Configuration to php artisan about
    This commit introduces a new section in the `php artisan about` command output, providing an overview of the Ninshiki configuration.
    
    This includes checks for:
    
    - Cloudinary setup
    - Reverb API key configuration
    - Resend API key configuration
    - Domain whitelist setup
    - Frontend URL configuration
    
    This enhancement aims to provide a quick and easy way for developers and administrators to verify the essential configurations of their Ninshiki application.

--- app/Providers/AboutInfoServiceProvider.php
+++ app/Providers/AboutInfoServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Spatie\Emoji\Emoji;
 
 class AboutInfoServiceProvider extends ServiceProvider
@@ -15,9 +16,9 @@ class AboutInfoServiceProvider extends ServiceProvider
     //https://unicode.org/Public/emoji/13.1/emoji-test.txt
     public static string $checked = "<fg=green;options=bold>\u{2714} Completed</>";
 
-    public static string $times = "<fg=red;options=bold>\u{274C}  Not Completed</>";
+    public static string $times = '<fg=red;options=bold>Not Setup</>';
 
-    public static string $maybe = "<fg=yellow;options=bold>\u{2753} Maybe</>";
+    public static string $maybe = '<fg=yellow;options=bold>Maybe</>';
 
     public function boot(): void
     {
@@ -30,6 +31,13 @@ class AboutInfoServiceProvider extends ServiceProvider
             'User Administrator' => fn () => User::with('roles')->get()->filter(
                 fn ($user) => $user->roles->where('name', 'Administrator')->toArray()
             )->count() > 0 ? self::$checked : self::$times,
+        ]);
+        AboutCommand::add('Ninshiki Configuration', static fn () => [
+            'Cloudinary' => Str::length(config('cloudinary.cloud_url')) > 0 ? self::$checked : self::$times,
+            'Reverb' => Str::length(env('REVERB_APP_KEY')) > 0 ? self::$checked : self::$times,
+            'Resend' => Str::length(env('RESEND_KEY')) > 0 ? self::$checked : self::$maybe,
+            'Domain Whitelist' => Str::length(env('ALLOWED_EMAIL_DOMAIN')) > 0 ? self::$checked : self::$times,
+            'Frontend' => Str::of(env('FRONTEND_URL'))->contains('localhost:3000') ? self::$maybe : self::$checked,
         ]);
     }
 }
