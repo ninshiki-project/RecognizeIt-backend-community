211fb8390a2b57b5ce2b168cee9baacbf6a77bab
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-23 20:01:14 +0800

    WIP

---
+++ tests/Feature/ProductControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Http\Controllers\Api;
+
+test('index', function () {});
+test('store', function () {});
+test('update', function () {});
+test('show', function () {});
+test('destroy', function () {});
