18c2ee8849edb9bc4058f12fae5b4297d6cba5d1
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-08-23 19:41:40 +0800

    ✨ feat(api): add shop endpoints
    Added endpoints for viewing, adding, and removing products from the shop.

---
+++ tests/Feature/ShopControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Http\Controllers\Api;
+
+use App\Models\Products;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+use function Pest\Laravel\deleteJson;
+use function Pest\Laravel\getJson;
+use function Pest\Laravel\postJson;
+
+it('can get all the product that are in the shop', function () {
+    getJson('/api/v1/shop')->assertStatus(200)
+        ->assertJson(fn (AssertableJson $json) => [
+            $json->has('data'),
+        ]);
+});
+it('can add a product to the shop', function () {
+    postJson('/api/v1/shop', [
+        'product' => Products::inRandomOrder()->first()?->id,
+    ])
+        ->assertStatus(201)
+        ->assertJson(fn (AssertableJson $json) => [
+            $json->has('data')
+                ->has('data.product'),
+        ]);
+});
+it('can remove the product in the shop', function () {
+    deleteJson('/api/v1/shop/'.Products::inRandomOrder()->first()->id)
+        ->assertStatus(204);
+});
