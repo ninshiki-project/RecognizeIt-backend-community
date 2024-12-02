69ffc9954d99e3db9f3cf52d5334e8967514a524
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-12-02 19:09:10 +0800

    ✨ feat(Models/PostingLimit.php): Update type hinting for `$fillable` property.

--- app/Models/PostingLimit.php
+++ app/Models/PostingLimit.php
@@ -14,7 +14,7 @@ class PostingLimit extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $fillable = [
         'designations_id',
