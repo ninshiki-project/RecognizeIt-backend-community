549d79d8e7effca3228705af3c2ef2931f865867
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-06 08:34:58 +0800

    ✨ feat(database): Make attachment type nullable in posts table.

--- app/Http/Requests/PostsPostRequest.php
+++ app/Http/Requests/PostsPostRequest.php
@@ -29,7 +29,7 @@ class PostsPostRequest extends FormRequest
             'recipient_id.*' => ['required', 'exists:users,id'],
             'post_content' => ['required', 'string'],
             'amount' => ['required_if:type,user', 'integer', 'in:3,5,10'],
-            'attachment_type' => ['required_if:type,user', Rule::in(['gif', 'image']), 'sometimes'],
+            'attachment_type' => [Rule::in(['gif', 'image']), 'sometimes'],
             'gif_url' => ['required_if:attachment_type,gif', 'url', 'sometimes'],
             'image' => ['required_if:attachment_type,image', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048', 'sometimes'],
             'type' => ['required', Rule::enum(PostTypeEnum::class)],
