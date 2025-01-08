b7690679c9d4e116b22b2381b1b01ac3c7fdff62
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-08 18:11:14 +0800

    ✨ feat(ShopResource): Implement view action and display product details in the shop resource.

--- app/Filament/Resources/ShopResource.php
+++ app/Filament/Resources/ShopResource.php
@@ -5,8 +5,11 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ShopResource\Pages;
 use App\Models\Shop;
 use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\Alignment;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables;
 use Filament\Tables\Table;
 
@@ -23,10 +26,25 @@ class ShopResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(1)
             ->schema([
+                FileUpload::make('image')
+                    ->visibleOn('view')
+                    ->image(),
                 Forms\Components\Select::make('product_id')
+                    ->native(false)
+                    ->preload()
+                    ->searchable()
                     ->relationship('product', 'name')
                     ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->visibleOn('view'),
+                Forms\Components\TextInput::make('stock')
+                    ->visibleOn('view'),
+                Forms\Components\TextInput::make('price')
+                    ->visibleOn('view'),
+                Forms\Components\TextInput::make('status')
+                    ->visibleOn('view'),
             ]);
     }
 
@@ -53,7 +71,19 @@ class ShopResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()
+                    ->mutateRecordDataUsing(function (array $data, Shop $shop): array {
+                        $data['name'] = $shop->product->name;
+                        $data['stock'] = $shop->product->stock;
+                        $data['price'] = $shop->product->price;
+                        $data['status'] = $shop->product->status;
+                        $data['image'] = $shop->product->image;
+
+                        return $data;
+                    })
+                    ->modalFooterActionsAlignment(Alignment::Right)
+                    ->modalWidth(MaxWidth::Small)
+                    ->modalAlignment(Alignment::Center),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
--- app/Filament/Resources/ShopResource/Pages/ManageShops.php
+++ app/Filament/Resources/ShopResource/Pages/ManageShops.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ShopResource\Pages;
 use App\Filament\Resources\ShopResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ManageRecords;
+use Filament\Support\Enums\Alignment;
+use Filament\Support\Enums\MaxWidth;
 
 class ManageShops extends ManageRecords
 {
@@ -13,7 +15,11 @@ class ManageShops extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->modalFooterActionsAlignment(Alignment::Right)
+                ->modalWidth(MaxWidth::Small)
+                ->modalAlignment(Alignment::Center)
+                ->createAnother(false),
         ];
     }
 }
