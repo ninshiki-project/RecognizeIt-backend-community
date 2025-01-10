43f282c472b47a9ddfb5241a70540385edf9a0ac
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-10 22:48:13 +0800

    ✅ test(`RedeemControllerTest.php`): Refactor redeem tests to improve reliability and add cancellation test.

--- tests/Feature/RedeemControllerTest.php
+++ tests/Feature/RedeemControllerTest.php
@@ -26,12 +26,11 @@ it('can display specific redeem record', function () {
 });
 it('can redeem from the shop', function () {
     $user = User::first();
-    $wallet = $user->wallet->deposit(400000);
-    $shop = Shop::whereHas('product', function ($query) {
-        $query->available();
-    })->inRandomOrder()->first();
+    $wallet = $user->getWallet('ninshiki-wallet');
+    $wallet->deposit(400000);
+    $shop = Shop::inRandomOrder()->first();
     $reps = postJson('/api/v1/redeems/shop', [
-        'shop' => $shop->id,
+        'shop' => $shop?->id,
     ])
         ->assertStatus(201)
         ->assertJsonStructure([
@@ -84,7 +83,9 @@ it('can display all the redeem item by status', function () {
         ]);
 });
 it('can update the status', function () {
-    patchJson('/api/v1/redeems/'.Redeem::inRandomOrder()->first()->id, [
+    $redeem = Redeem::where('status', '!=', RedeemStatusEnum::DECLINED)->where('status', '!=', RedeemStatusEnum::REDEEMED)->first();
+
+    patchJson('/api/v1/redeems/'.$redeem->id, [
         'status' => RedeemStatusEnum::REDEEMED->value,
     ])
         ->assertStatus(200)
@@ -98,7 +99,23 @@ it('can update the status', function () {
         ]);
 });
 it('can cancel the redeem item from shop if still in waiting for approval', function () {
-    $redeem = Redeem::where('status', RedeemStatusEnum::WAITING_APPROVAL->value)->first();
+    $user = User::first();
+    $wallet = $user->getWallet('ninshiki-wallet');
+    $wallet->deposit(400000);
+    $shop = Shop::inRandomOrder()->first();
+    $reps = postJson('/api/v1/redeems/shop', [
+        'shop' => $shop?->id,
+    ])
+        ->assertStatus(201)
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'user',
+                'product',
+                'status',
+            ],
+        ]);
+    $redeem = Redeem::where('status', '=', RedeemStatusEnum::WAITING_APPROVAL)->first();
     deleteJson('/api/v1/redeems/'.$redeem->id)
         ->assertOk()
         ->assertJsonStructure([
