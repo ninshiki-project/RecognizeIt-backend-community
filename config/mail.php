75293956a397155f52b9ddc86f7e81acb9fc0036
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-10 18:41:19 +0800

    ✨ feat(`config/mail.php`): Update mail configuration to remove local domain and use null for timeout.

--- config/mail.php
+++ config/mail.php
@@ -42,11 +42,10 @@ return [
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
@@ -88,7 +87,7 @@ return [
             'mailers' => [
                 'resend',
                 'sendmail',
-                'smtp'
+                'smtp',
             ],
         ],
 
