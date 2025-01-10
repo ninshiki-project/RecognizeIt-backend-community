e412f9033d05fc66a8c2e563f5e9765cc9039ff5
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-10 13:44:10 +0800

    ✨ feat(database): Add `decline_reason` and `decline_date` columns to the `redeems` table.

---
+++ database/migrations/2025_01_10_054205_add_decline_reason_and_date_to_redeems.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('redeems', function (Blueprint $table) {
+            $table->after('status', function () use ($table) {
+                $table->text('decline_reason')->nullable();
+                $table->timestamp('decline_date')->nullable();
+            });
+
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('redeems', function (Blueprint $table) {
+            $table->dropColumn('decline_reason');
+            $table->dropColumn('decline_date');
+        });
+    }
+};
