1772074dc26054901b84a102b87702c657a084c8
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-10 18:41:56 +0800

    ✨ feat(RedeemResource): Implement redeem approval, processing, and decline actions.  Adds actions to approve, process, redeem, and decline redeem requests in the Filament resource.

--- app/Filament/Resources/RedeemResource.php
+++ app/Filament/Resources/RedeemResource.php
@@ -2,12 +2,15 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Actions\RedeemDeclined;
 use App\Filament\Resources\RedeemResource\Pages;
+use App\Http\Controllers\Api\Enum\RedeemDeclineReasonEnum;
 use App\Http\Controllers\Api\Enum\RedeemStatusEnum;
 use App\Models\Redeem;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\ActionSize;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -51,6 +54,7 @@ class RedeemResource extends Resource
             ->modifyQueryUsing(fn (Builder $query) => $query->withoutGlobalScopes())
             ->emptyStateHeading('No Data Available')
             ->emptyStateDescription('Once an employee redeem a product from the shop, it will appear here.')
+            ->actionsPosition(Tables\Enums\ActionsPosition::BeforeColumns)
             ->columns([
                 Tables\Columns\TextColumn::make('id')
                     ->label('Transaction ID')
@@ -91,7 +95,88 @@ class RedeemResource extends Resource
                 //
             ])
             ->actions([
-                //
+                Tables\Actions\Action::make('Approve')
+                    ->visible(fn (Redeem $redeem) => $redeem->status === RedeemStatusEnum::WAITING_APPROVAL)
+                    ->label('Approve')
+                    ->hiddenLabel()
+                    ->size(ActionSize::Medium)
+                    ->icon(RedeemStatusEnum::APPROVED->getIcon())
+                    ->tooltip('Approve Redeem')
+                    ->requiresConfirmation()
+                    ->action(function (Redeem $redeem) {
+                        $redeem->status = RedeemStatusEnum::APPROVED;
+                        $redeem->save();
+                    })
+                    ->modalIcon(RedeemStatusEnum::APPROVED->getIcon()),
+                Tables\Actions\Action::make('Processing')
+                    ->visible(fn (Redeem $redeem) => $redeem->status === RedeemStatusEnum::APPROVED)
+                    ->label('Processing')
+                    ->color('warning')
+                    ->hiddenLabel()
+                    ->size(ActionSize::Medium)
+                    ->icon(RedeemStatusEnum::PROCESSING->getIcon())
+                    ->tooltip('Process Redeem')
+                    ->requiresConfirmation()
+                    ->action(function (Redeem $redeem) {
+                        $redeem->status = RedeemStatusEnum::PROCESSING;
+                        $redeem->save();
+                    })
+                    ->modalIcon(RedeemStatusEnum::PROCESSING->getIcon()),
+                Tables\Actions\Action::make('Redeemed')
+                    ->visible(fn (Redeem $redeem) => $redeem->status === RedeemStatusEnum::APPROVED || $redeem->status === RedeemStatusEnum::PROCESSING)
+                    ->color('secondary')
+                    ->label('Redeemed')
+                    ->hiddenLabel()
+                    ->size(ActionSize::Medium)
+                    ->icon(RedeemStatusEnum::REDEEMED->getIcon())
+                    ->tooltip('Redeemed')
+                    ->requiresConfirmation()
+                    ->action(function (Redeem $redeem) {
+                        $redeem->status = RedeemStatusEnum::REDEEMED;
+                        $redeem->save();
+                    })
+                    ->modalIcon(RedeemStatusEnum::REDEEMED->getIcon()),
+                Tables\Actions\Action::make('decline')
+                    ->visible(fn (Redeem $redeem) => $redeem->status === RedeemStatusEnum::WAITING_APPROVAL)
+                    ->label('Decline')
+                    ->hiddenLabel()
+                    ->color('danger')
+                    ->size(ActionSize::Medium)
+                    ->icon(RedeemStatusEnum::DECLINED->getIcon())
+                    ->tooltip('Decline Redeem')
+                    ->requiresConfirmation()
+                    ->modalIcon(RedeemStatusEnum::DECLINED->getIcon())
+                    ->form([
+                        Forms\Components\Select::make('category')
+                            ->required()
+                            ->native(false)
+                            ->preload()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, Forms\Set $set) {
+                                if (is_null($state)) {
+                                    $set('description', '');
+                                }
+                                switch ($state) {
+                                    case RedeemDeclineReasonEnum::ITEM_NO_LONGER_AVAILABLE->getLabel():
+                                        $set('description', RedeemDeclineReasonEnum::ITEM_NO_LONGER_AVAILABLE->getDescription());
+                                        break;
+                                    case RedeemDeclineReasonEnum::INCORRECT_ITEM_PRICING->getLabel():
+                                        $set('description', RedeemDeclineReasonEnum::INCORRECT_ITEM_PRICING->getDescription());
+                                        break;
+                                    case RedeemDeclineReasonEnum::EXCEED_REDEMPTION_LIMIT->getLabel():
+                                        $set('description', RedeemDeclineReasonEnum::EXCEED_REDEMPTION_LIMIT->getDescription());
+                                        break;
+                                    case RedeemDeclineReasonEnum::ITEM_TEMPORARILY_UNAVAILABLE->getLabel():
+                                        $set('description', RedeemDeclineReasonEnum::ITEM_TEMPORARILY_UNAVAILABLE->getDescription());
+                                        break;
+                                }
+                            })
+                            ->options(RedeemDeclineReasonEnum::class),
+                        Forms\Components\Textarea::make('description')
+                            ->rows(5)
+                            ->required(),
+                    ])
+                    ->action(fn (array $data, Redeem $record, RedeemDeclined $redeemDeclined) => $redeemDeclined->handle($record, $data)),
             ])
             ->bulkActions([
                 //
