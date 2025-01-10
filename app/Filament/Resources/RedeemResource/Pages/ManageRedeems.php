cd020b6e3a389ad39cfdf14bd3a65e86c354239c
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-10 11:34:27 +0800

    ✨ feat(RedeemResource): Implement status-based tabs for redeem management.

--- app/Filament/Resources/RedeemResource/Pages/ManageRedeems.php
+++ app/Filament/Resources/RedeemResource/Pages/ManageRedeems.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\RedeemResource\Pages;
 
 use App\Filament\Resources\RedeemResource;
+use App\Http\Controllers\Api\Enum\RedeemStatusEnum;
+use Filament\Resources\Components;
 use Filament\Resources\Pages\ManageRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ManageRedeems extends ManageRecords
 {
@@ -12,6 +15,41 @@ class ManageRedeems extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
+        ];
+    }
+
+    public function getDefaultActiveTab(): string|int|null
+    {
+        return 'For Approval';
+    }
+
+    public function getTabs(): array
+    {
+
+        return [
+            'all' => Components\Tab::make()
+                ->label('All Redeems')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '!=', RedeemStatusEnum::CANCELED)),
+            'For Approval' => Components\Tab::make()
+                ->label('Awaiting Approval')
+                ->icon(RedeemStatusEnum::WAITING_APPROVAL->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', RedeemStatusEnum::WAITING_APPROVAL)),
+            'Approved' => Components\Tab::make()
+                ->label('Approved')
+                ->icon(RedeemStatusEnum::APPROVED->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', RedeemStatusEnum::APPROVED)),
+            'Processing' => Components\Tab::make()
+                ->label('Processing')
+                ->icon(RedeemStatusEnum::PROCESSING->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', RedeemStatusEnum::PROCESSING)),
+            'Redeemed' => Components\Tab::make()
+                ->label('Redeemed')
+                ->icon(RedeemStatusEnum::REDEEMED->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', RedeemStatusEnum::REDEEMED)),
+            'Declined' => Components\Tab::make()
+                ->label('Declined')
+                ->icon(RedeemStatusEnum::DECLINED->getIcon())
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', '=', RedeemStatusEnum::DECLINED)),
         ];
     }
 }
