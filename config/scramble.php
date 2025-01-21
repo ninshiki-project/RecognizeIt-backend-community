f698f9e72b2d40cab3787c04145bcb5fd26aa45b
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-01-21 15:37:46 +0800

    ✨ feat(`config/scramble.php`): Update API documentation description and add logo.

--- config/scramble.php
+++ config/scramble.php
@@ -29,7 +29,7 @@ return [
         /*
          * Description rendered on the home page of the API documentation (`/docs/api`).
          */
-        'description' => 'Ninshiki API Documentation',
+        'description' => file_get_contents(__DIR__.'/../README.md'),
     ],
 
     /*
@@ -54,7 +54,7 @@ return [
         /*
          * URL to an image that displays as a small square logo next to the title, above the table of contents.
          */
-        'logo' => '',
+        'logo' => '/Ninshiki_app-transparent.png',
 
         /*
          * Use to fetch the credential policy for the Try It feature. Options are: omit, include (default), and same-origin
