2f9eac89bc9d3f4706363d499fdea31999bc63bc
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-02-13 19:22:32 +0800

    ✨ feat(API): Add application maintenance endpoint.  This endpoint allows checking if the API is currently under maintenance.

--- app/Http/Controllers/Api/ApplicationController.php
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ApplicationResource;
 use App\Models\Application;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Cache;
 
@@ -22,5 +23,21 @@ class ApplicationController extends Controller
         return Cache::flexible(static::$cacheKey, [5, 10], function () {
             return ApplicationResource::make(Application::first());
         });
+    }
+
+    /**
+     * Application Maintenance
+     *
+     * Check if API is under maintenance
+     *
+     * @unauthenticated
+     *
+     * @return JsonResponse
+     */
+    public function isMaintenance()
+    {
+        return response()->json([
+            'maintenance_mode' => Application::first()->more_configs['maintenance']['maintenance_mode'] ?? false,
+        ]);
     }
 }
