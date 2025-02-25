575d0aa1e1f4ef381ea15b90d9f1eddd6e5e90da
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-02-25 14:03:56 +0800

    ♻️ refactor(`routes/web.php`): Refactor API routes to only respond on local environment.

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 if (\Composer\InstalledVersions::isInstalled('ninshiki-project/ninshiki')) {
-    Route::get('/api', function () {
-        return response()->json([
-            'laravel' => app()->version(),
-            'documentation' => '/docs/api',
-            'documentation_version' => config('scramble.info.version'),
-            'php_version' => PHP_VERSION,
-        ]);
-    });
-    Route::get('/', function () {
-        return response()->json([
-            'environment' => app()->environment(),
-            'laravel' => app()->version(),
-            'php_version' => PHP_VERSION,
-        ]);
-    });
-
+    if (app()->isLocal()) {
+        Route::get('/api', function () {
+            return response()->json([
+                'laravel' => app()->version(),
+                'documentation' => '/docs/api',
+                'documentation_version' => config('scramble.info.version'),
+                'php_version' => PHP_VERSION,
+            ]);
+        });
+    }
 } else {
-    Route::get('/', function () {
-        return response()->json([
-            'laravel' => app()->version(),
-            'documentation' => '/docs/api',
-            'documentation_version' => config('scramble.info.version'),
-            'php_version' => PHP_VERSION,
-        ]);
-    });
+    if (app()->isLocal()) {
+        Route::get('/', function () {
+            return response()->json([
+                'laravel' => app()->version(),
+                'documentation' => '/docs/api',
+                'documentation_version' => config('scramble.info.version'),
+                'php_version' => PHP_VERSION,
+            ]);
+        });
+    }
 }
